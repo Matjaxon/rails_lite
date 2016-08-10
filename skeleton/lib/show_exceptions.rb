e0f2ce3860a840426d2167f4d913bb12d48e0ea6
Matjaxon <MattRJackson1@gmail.com>
2016-08-10 15:58:59 -0700

    Added exception handling middleware to redirect in the event a StandardError occurs

--- skeleton/lib/show_exceptions.rb
+++ skeleton/lib/show_exceptions.rb
@@ -1,15 +1,37 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    puts "Initializing Exception Handler"
+    @app = app
   end
 
   def call(env)
+    puts "Calling Exception Handler"
+    begin
+      app.call(env)
+    rescue StandardError => e
+      @res = Rack::Response.new
+      @res.status = 500
+      render_exception(e)
+      @res.finish
+
+      # Array below is for spec clearing.  Comment out to see actual error page.
+      # ['500', {'Content-type' => 'text/html'}, @e.message]
+    end
   end
 
   private
 
   def render_exception(e)
+    @e = e
+    current_path = File.dirname(__FILE__).to_s
+    rescue_path = File.join(current_path, "templates", "rescue.html.erb")
+    content = ERB.new(File.read(rescue_path)).result(binding)
+    @res["Content-Type"] = "text/html"
+    @res.write(content)
   end
 
 end
